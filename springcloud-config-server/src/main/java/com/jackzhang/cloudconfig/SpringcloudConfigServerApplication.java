8571b0623ba1b6d902f64bdb09557cd3804e2433
JackZhang <1021938637@qq.com>
2017-12-03 18:53:50 +0800

    cloud config

---
+++ springcloud-config-server/src/main/java/com/jackzhang/cloudconfig/SpringcloudConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.jackzhang.cloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudConfigServerApplication.class, args);
+	}
+}
